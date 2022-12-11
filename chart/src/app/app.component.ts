3a8fa36315a4f08afe19808539039d41d0aa9f28
vickysrini <maxivicky143@gmail.com>
2022-12-11 21:13:28 +0530

    org-chart

---
+++ chart/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import OrgChart from "@balkangraph/orgchart.js";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'chart';
+
+  constructor() { }
+
+  ngOnInit() {
+    const tree = document.getElementById('tree');
+    if (tree) {
+      var chart = new OrgChart(tree, {
+        nodeBinding: {
+          field_0: "name",
+          img_0: "img"
+        },
+      });
+
+      chart.load([
+        { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+        { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
+        { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+        { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
+        { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
+        { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
+        { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+      ]);
+    }
+  }
+}
